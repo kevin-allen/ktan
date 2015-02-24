395117a427e52a6de9396dd942375497ac181efb
Kevin Allen <allen@uni-heidelberg.de>
2015-02-24 15:43:38 +0100

    starting to add signals to mainWindow

--- src/mainWindow.cpp
+++ src/mainWindow.cpp
@@ -1,3 +1,4 @@
+#define DEBUG_WIN
 #include "mainWindow.h"
 #include "rhd2000datablock.h"
 #include "rhd2000registers.h"
@@ -12,10 +13,39 @@ mainWindow::mainWindow(BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder>
   Gtk::Window(cobject), builder(refGlade) // call Gtk::Window and builder
 {
 
+#ifdef DEBUG_WIN
+  cerr << "entering mainWindow::mainWindow()\n";
+#endif
+
   cerr << "entering mainWindow::mainWindow()\n";
   db = new dataBuffer; // buffer that holds the latest data acquired by acquisition object
   acq = new acquisition(db); // pass a dataBuffer as a pointer to the acquisition object
   rec = new recording(db); // pass a dataBuffer as a pointer to the recording object
+
+  // get the widget from builder
+  builder->get_widget("play_toolbutton",play_toolbutton);
+  builder->get_widget("record_toolbutton",record_toolbutton);
+  builder->get_widget("rewind_toolbutton",rewind_toolbutton);
+  builder->get_widget("forward_toolbutton",forward_toolbutton);
+  builder->get_widget("gain_increase_toolbutton",gain_increase_toolbutton);
+  builder->get_widget("gain_decrease_toolbutton",gain_decrease_toolbutton);
+  builder->get_widget("time_increase_toolbutton",time_increase_toolbutton);
+  builder->get_widget("time_decrease_toolbutton",time_decrease_toolbutton);
+  builder->get_widget("about_menuitem",about_menuitem);
+  builder->get_widget("about_dialog",about_dialog);
+  
+  
+  // connect signals to functions
+  play_toolbutton->signal_toggled().connect(sigc::mem_fun(*this, &mainWindow::on_play_toolbutton_toggled));
+  record_toolbutton->signal_toggled().connect(sigc::mem_fun(*this, &mainWindow::on_record_toolbutton_toggled));
+  rewind_toolbutton->signal_clicked().connect(sigc::mem_fun(*this, &mainWindow::on_rewind_toolbutton_clicked));
+  forward_toolbutton->signal_clicked().connect(sigc::mem_fun(*this, &mainWindow::on_forward_toolbutton_clicked));
+  gain_increase_toolbutton->signal_clicked().connect(sigc::mem_fun(*this, &mainWindow::on_gain_increase_toolbutton_clicked));
+  gain_decrease_toolbutton->signal_clicked().connect(sigc::mem_fun(*this, &mainWindow::on_gain_decrease_toolbutton_clicked));
+  time_increase_toolbutton->signal_clicked().connect(sigc::mem_fun(*this, &mainWindow::on_time_increase_toolbutton_clicked));
+  time_decrease_toolbutton->signal_clicked().connect(sigc::mem_fun(*this, &mainWindow::on_time_decrease_toolbutton_clicked));
+  about_menuitem->signal_activate().connect(sigc::mem_fun(*this, &mainWindow::on_about_menuitem_activate));
+
 
   // // start data acquisition on the board
   // acq->start_acquisition();
@@ -29,17 +59,137 @@ mainWindow::mainWindow(BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder>
   // acq->stop_acquisition();
   // rec->stop_recording();
 
+
+#ifdef DEBUG_WIN
   cerr << "leaving mainWindow::mainWindow()\n";
+#endif
+
+
 }
 
 mainWindow::~mainWindow()
 {
+#ifdef DEBUG_WIN
   cerr << "entering mainWindow::~mainWindow()\n";
+#endif
+
   delete acq;
   delete db;
   delete rec;
+
+
+#ifdef DEBUG_WIN
   cerr << "leaving mainWindow::~mainWindow()\n";
+#endif
 
 }
 
 
+void mainWindow::on_play_toolbutton_toggled()
+{
+#ifdef DEBUG_WIN
+  cerr << "entering mainWindow::on_play_toolbutton_toggled()\n";
+#endif
+
+#ifdef DEBUG_WIN
+  cerr << "leaving mainWindow::on_play_toolbutton_toggled()\n";
+#endif
+}
+void mainWindow::on_record_toolbutton_toggled()
+{
+#ifdef DEBUG_WIN
+  cerr << "entering mainWindow::on_record_toolbutton_toggled()\n";
+#endif
+
+#ifdef DEBUG_WIN
+  cerr << "leaving mainWindow::on_record_toolbutton_toggled()\n";
+#endif
+}
+void mainWindow::on_rewind_toolbutton_clicked()
+{
+#ifdef DEBUG_WIN
+  cerr << "entering mainWindow::on_rewind_toolbutton_clicked()\n";
+#endif
+
+#ifdef DEBUG_WIN
+  cerr << "leaving mainWindow::on_rewind_toolbutton_clicked()\n";
+#endif
+  
+}
+void mainWindow::on_forward_toolbutton_clicked()
+{
+#ifdef DEBUG_WIN
+  cerr << "entering mainWindow::on_forward_toolbutton_clicked()\n";
+#endif
+
+#ifdef DEBUG_WIN
+  cerr << "leaving mainWindow::on_forward_toolbutton_clicked()\n";
+#endif
+}
+void mainWindow::on_gain_increase_toolbutton_clicked()
+{
+#ifdef DEBUG_WIN
+  cerr << "entering mainWindow::on_gain_increase_toolbutton_clicked()\n";
+#endif
+
+#ifdef DEBUG_WIN
+  cerr << "leaving mainWindow::on_gain_increase_toolbutton_clicked()\n";
+#endif
+}
+void mainWindow::on_gain_decrease_toolbutton_clicked()
+{
+#ifdef DEBUG_WIN
+  cerr << "entering mainWindow::on_gain_decrease_toolbutton_clicked()\n";
+#endif
+
+#ifdef DEBUG_WIN
+  cerr << "leaving mainWindow::on_gain_decrease_toolbutton_clicked()\n";
+#endif
+}
+void mainWindow::on_time_increase_toolbutton_clicked()
+{
+#ifdef DEBUG_WIN
+  cerr << "entering mainWindow::on_time_increase_toolbutton_clicked()\n";
+#endif
+
+#ifdef DEBUG_WIN
+  cerr << "leaving mainWindow::on_time_increase_toolbutton_clicked()\n";
+#endif  
+}
+void mainWindow::on_time_decrease_toolbutton_clicked()
+{
+#ifdef DEBUG_WIN
+  cerr << "entering mainWindow::on_time_decrease_toolbutton_clicked()\n";
+#endif
+
+#ifdef DEBUG_WIN
+  cerr << "leaving mainWindow::on_time_decrease_toolbutton_clicked()\n";
+#endif
+}
+void mainWindow::on_about_menuitem_activate()
+{
+#ifdef DEBUG_WIN
+  cerr << "entering mainWindow::on_about_menuitem_activate()\n";
+#endif
+
+  int response;
+  response=about_dialog->run();
+  switch(response)
+    {
+    case (GTK_RESPONSE_CANCEL):
+      {
+	about_dialog->hide();
+	break;
+      }
+    default:
+      {
+	about_dialog->hide();
+	break;
+      }
+    }
+
+
+#ifdef DEBUG_WIN
+  cerr << "leaving mainWindow::on_about_menu_item_activate()\n";
+#endif
+}
--- src/mainWindow.h
+++ src/mainWindow.h
@@ -18,14 +18,93 @@ class mainWindow: public Gtk::Window
   int acquisition_thread_id;
   pthread_t recording_thread;
   int recording_thread_id;
-
+  
  public:
   mainWindow(BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder>& refGlade);//constructor
   virtual ~mainWindow();
   
  protected:
   Glib::RefPtr<Gtk::Builder> builder;
-  
+
+  // widgets
+  Gtk::ToggleToolButton* play_toolbutton;
+  Gtk::ToggleToolButton* record_toolbutton;
+  Gtk::ToolButton*  rewind_toolbutton;
+  Gtk::ToolButton*  forward_toolbutton;
+  Gtk::ToolButton*  gain_increase_toolbutton;
+  Gtk::ToolButton*  gain_decrease_toolbutton;
+  Gtk::ToolButton*  time_increase_toolbutton;
+  Gtk::ToolButton*  time_decrease_toolbutton;
+
+  Gtk::MenuItem*  about_menuitem;
+  Gtk::AboutDialog* about_dialog;
+
+
+
+  /* GtkWidget *window;  // main window */
+  /* GtkWidget *vbox1; // main vbox in the main window, to put the gtkdatabox */
+  /* GtkWidget *test_label; */
+  /* GtkWidget *drawing_area; */
+  /* GdkColor color; */
+  /* GtkAdjustment *sampling_rate_adjustment; */
+  /* GtkAdjustment *osc_group_adjustment; */
+  /* GtkAdjustment *osc_group_preferences_adjustment; */
+  /* GtkAdjustment *trial_no_adjustment; */
+  /* GtkWidget *about_dlg; // about dialog */
+  /* GtkWidget *acquisition_dlg; */
+  /* GtkWidget *oscilloscope_dlg; */
+  /* GtkWidget *recording_dlg; */
+  /* GtkWidget *toolbar; */
+  /* GtkWidget *dev1_name_label; // to show user the name of device */
+  /* GtkWidget *dev2_name_label; // to show user the driver */
+  /* GtkWidget *dev1_driver_label; */
+  /* GtkWidget *dev2_driver_label; */
+  /* GtkWidget *num_devices_detected_label; */
+  /* GtkWidget *sampling_rate_value_label; */
+  /* GtkWidget *num_available_channels_label; */
+  /* GtkWidget *num_channels_device_1_label; */
+  /* GtkWidget *num_channels_device_2_label; */
+  /* GtkWidget *range_label; */
+  /* GtkWidget *current_saving_directory_label2; */
+  /* GtkWidget *group_preferences_spinbutton; */
+  /* GtkWidget *preferences_channel_vbox; // to display the channel information */
+  /* GtkWidget *file_name_entry; // filebase of the file name */
+  /* GtkWidget *trial_spinbutton; // index following filebase for file name */
+  /* GtkWidget *statusbar; // index following filebase for file name    */
+  /* GtkWidget *group_spinbutton; // for oscilloscope display group */
+  /* GtkWidget *recording_channel_view; // treeview to select the channels to record */
+  /* GtkWidget *time_decrease_image; */
+  /* GtkWidget *time_increase_image; */
+  /* GtkWidget *gain_decrease_image; */
+  /* GtkWidget *gain_increase_image; */
+  /* GtkWidget *time_decrease_toolbutton; */
+  /* GtkWidget *time_increase_toolbutton; */
+  /* GtkWidget *gain_decrease_toolbutton; */
+  /* GtkWidget *gain_increase_toolbutton; */
+
+  /* GtkListStore  *recording_channel_store; //to fill up the treeview to select the recording channels */
+  /* GtkWidget *oscilloscope_all_channels_view; // treeview to select the channels to record */
+  /* GtkListStore  *oscilloscope_all_channels_store; // to fill the treeview to select the channels for oscilloscope */
+  /* GtkWidget *oscilloscope_selected_channels_view; // selected channels for a page of the oscilloscope */
+  /* GtkListStore *oscilloscope_selected_channels_store; // to fill the treeview for a group of the oscilloscope */
+  /* int is_filling_selected_channel_liststore; */
+
+
+
+
+
+  // callback functions
+  void on_play_toolbutton_toggled();
+  void on_record_toolbutton_toggled();
+  void on_rewind_toolbutton_clicked();
+  void on_forward_toolbutton_clicked();
+  void on_gain_increase_toolbutton_clicked();
+  void on_gain_decrease_toolbutton_clicked();
+  void on_time_increase_toolbutton_clicked();
+  void on_time_decrease_toolbutton_clicked();
+  void on_about_menuitem_activate();
+
+
 };
 
 #endif // MAINWINDOW_H
